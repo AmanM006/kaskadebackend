7f26373c0c24e4c737a944394c5497927d7dc35f
Aman <82795889+AmanM006@users.noreply.github.com>
2025-03-29 14:14:46 +0530

    Update LoyaltyTrends.js

--- models/LoyaltyTrends.js
+++ models/LoyaltyTrends.js
@@ -1,1 +1,17 @@
+// const mongoose = require("mongoose");
+
+// const LoyaltyTrendsSchema = new mongoose.Schema({
+//     year: { type: Number, required: true },
+//     month: { type: Number, required: true },
+//     value: { type: Number, required: true } // Loyalty score or percentage
+// });
+
+// const LoyaltyTrendsModel = mongoose.model("LoyaltyTrends", LoyaltyTrendsSchema);
+
+// module.exports = LoyaltyTrendsModel;
+// db.loyaltytrends.insertMany([
+//     { year: 2024, month: 3, value: 78.5 },
+//     { year: 2024, month: 2, value: 74.3 },
+//     { year: 2024, month: 1, value: 69.2 }
+// ]);
 
